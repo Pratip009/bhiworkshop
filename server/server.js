138f918f7c1c7b13596c800f2787d14e24cc46bd
Pratip Kayal <pratipkayal1@gmail.com>
2025-09-16 21:40:33 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -94,7 +94,7 @@ app.get("*", (req, res) => {
 // ---------------------------------------------------------
 
 // ------------------- START SERVER -------------------
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.REACT_APP_API_URL || 8000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
